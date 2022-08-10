b1f3a0aa3ac7c2d63140cd7384a6e950e8c20608
tin <trung.tin.nguyen0309@gmail.com>
2022-08-10 20:51:59 +0700

    Increase shared memory to 1GB

--- run_triton.sh
+++ run_triton.sh
@@ -1,3 +1,3 @@
-docker run --gpus=1 --rm -p8000:8000 -p8001:8001 -p8002:8002 \
-    -v <full-path-to-model-repository>:/models \
+docker run --gpus=1 --rm -p8000:8000 -p8001:8001 -p8002:8002 --shm-size=1gb\
+    -v /home/tin/Workspace/triton/deploy-detectron2-with-triton/models:/models \
     nvcr.io/nvidia/tritonserver:22.07-py3 tritonserver --model-repository=/models
